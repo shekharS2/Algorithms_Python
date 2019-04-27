ef429cad6ea5bc301887ec35ecbb87d44cc80006
Shekhar Saikia <saikiashekhar25@gmail.com>
2019-04-27 11:51:55 +0530

    Initial commit

---
+++ venv/CatalanNo.py
@@ -0,0 +1,16 @@
+cat = [-100] * 200
+
+def catalan(n):
+    if n <= 1:
+        cat[n] = 1
+        return cat[n]
+    elif cat[n] != -100:
+        return cat[n]
+    else:
+        for i in range(0, n):
+            cat[n] = cat[n-1] + catalan(i) * catalan(n-1-i)
+        return cat[n]
+
+catalan(30)
+
+print(cat)
